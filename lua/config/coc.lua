f41356b9cd1f0f5a5c6702298342169f3d7ac04e
Qiming Chu <cchuqiming@gmail.com>
2023-12-30 23:53:20 +0800

    Remove coc_sh

--- lua/config/coc.lua
+++ lua/config/coc.lua
@@ -1,6 +1,6 @@
 vim.cmd 
 [[
-	let g:coc_global_extensions = ['coc-json', 'coc-git', 'coc-clangd', 'coc-cmake', 'coc-highlight', 'coc-jedi', 'coc-sh', 'coc-snippets', 'coc-vimlsp', 'coc-pairs']
+	let g:coc_global_extensions = ['coc-json', 'coc-git', 'coc-clangd', 'coc-cmake', 'coc-highlight', 'coc-jedi', 'coc-snippets', 'coc-vimlsp', 'coc-pairs']
 ]]
 -- Some servers have issues with backup files, see #649
 vim.opt.backup = false
